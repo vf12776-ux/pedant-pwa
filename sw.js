7ce905bc029216b35081b4efae81d427600d2058
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-08-12 23:46:00 +0300

    Заголовок Pedant.ru + Service Worker v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pedant-pwa-v2';
+const CACHE_NAME = 'pedant-pwa-v3';
 const urlsToCache = [
   '/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/offline.html'
