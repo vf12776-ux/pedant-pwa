e4c4d07f90d99c166be2d55fbb1658848cd052c8
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-08-13 01:10:32 +0300

    Улучшена установка: определение WebView + подсветка кнопки

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pedant-pwa-v7';
+const CACHE_NAME = 'pedant-pwa-v8';
 const urlsToCache = [
   '/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/offline.html'
