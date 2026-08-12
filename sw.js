14c18834dc4eff16e4c23b536b21af18ad6dfd35
vf12776-ux <vf12776-ux@users.noreply.github.com>
2026-08-12 23:14:41 +0300

    Обновлена палитра pedant.ru + Service Worker v2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'pedant-pwa-v1';
+const CACHE_NAME = 'pedant-pwa-v2';
 const urlsToCache = [
   '/', '/index.html', '/manifest.json',
   '/icons/icon-192.png', '/icons/icon-512.png', '/offline.html'
